959e763288cb63435db2bcf6ea4ece8c8fba7608
stevenAnto <ecalcinap@unsa.edu.pe>
2023-04-27 15:26:08 -0500

    use of LocalStorage

--- counter.js
+++ counter.js
@@ -1,13 +1,18 @@
-let counter =0;
+if (!localStorage.getItem('counter')){
+  localStorage.setItem('counter',0);
+
+}
 function count(){
-  counter ++;
+  let counter= localStorage.getItem('counter');
+  counter++;
   document.querySelector('h1').innerHTML = counter;
+  localStorage.setItem('counter',counter);
 }
 //Para esperar a que cargue todo
 //DOMContentLoaded 
 document.addEventListener('DOMContentLoaded',function (){
+  document.querySelector('h1').innerHTML=localStorage.getItem('counter');
   document.querySelector('button').addEventListener('click',count)
-  setInterval(count,1000);
   //document.querySelector('button').onclick =count;
 
 });
