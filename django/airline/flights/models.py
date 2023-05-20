a6de75f247de73275b31e1ed2d18b6ee76cf3f31
stevenAnto <ecalcinap@unsa.edu.pe>
2023-05-20 16:14:43 -0500

    primer modelo de flights

---
+++ django/airline/flights/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+    des __str__(self):
+        return f"{self.city} ({self.code})"
+class Flight(models.Model):
+    origin = models.CharField(max_length=64)
+    destination = models.CharField(max_length=64)
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}:{self.origin}to{self.destination}"
+
+# Create your models here.
