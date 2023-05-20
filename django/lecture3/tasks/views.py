71de2a9ad2960d97b76639518a5f10992ae11c6b
stevenAnto <ecalcinap@unsa.edu.pe>
2023-05-20 10:26:05 -0500

    add redirection with HttpResponseRedirect

--- django/lecture3/tasks/views.py
+++ django/lecture3/tasks/views.py
@@ -1,17 +1,19 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django import forms
 from django.shortcuts import render
 
 
-tasks = ["foo","bar","baz"]
+tasks = []
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Tasks")
-    priority = forms.IntegerField(label="Priorityu",min_value=1,max_value=10)
 
 def index(request):
     return render(request,"tasks/index.html",{"tasks":tasks})
 def add(request):
     #si el metodo es post, o se esta enviando. Lo que se hace es crear un nuevo formulario con toda la informacion
     #de lo que se esta enviando
+    #si es POST entonces copiar a la list
     if request.method == "POST":
         #creo un nuevo formulario con toda la data
         form = NewTaskForm(request.POST)
@@ -21,8 +23,11 @@ def add(request):
             task=form.cleaned_data["task"]
             #lo agrego a la lista
             tasks.append(task)
+            #Automaticamente refresa a la otra pagina
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request,"tasks/add.html",{"form":form})
+    # pase cualquier cosa, igual renderizar
     return render(request,"tasks/add.html",{"form":NewTaskForm()})
 
 
