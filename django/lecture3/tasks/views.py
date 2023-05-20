cbb3267255ff92c0f0cdc8cded378dfd4bc60449
stevenAnto <ecalcinap@unsa.edu.pe>
2023-05-19 21:54:31 -0500

    To do List

---
+++ django/lecture3/tasks/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+tasks = ["foo","bar","baz"]
+def index(request):
+    return render(request,"tasks/index.html",{
+        "tasks":tasks
+        }
+            )
+
+
+# Create your views here.
