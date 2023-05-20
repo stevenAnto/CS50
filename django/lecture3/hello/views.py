dfe24d38f7c88ba979f186893646f4d055c41336
stevenAnto <ecalcinap@unsa.edu.pe>
2023-05-19 20:22:23 -0500

    programa que recibe parametros

--- django/lecture3/hello/views.py
+++ django/lecture3/hello/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render
 def index(request):
     return HttpResponse("Hello")
 
+def esteven(request):
+    return HttpResponse("Helo, Esteven")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name}")
+
 # Create your views here.
