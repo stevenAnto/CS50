ce57e26f3be8a12f06bda885226d4cd86e97e20e
stevenAnto <ecalcinap@unsa.edu.pe>
2023-05-20 11:27:56 -0500

    se usa request.session, pero no acepta append, q extranio

--- django/lecture3/tasks/views.py
+++ django/lecture3/tasks/views.py
@@ -4,13 +4,17 @@ from django import forms
 from django.shortcuts import render
 
 
-tasks = []
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Tasks")
 
 def index(request):
-    return render(request,"tasks/index.html",{"tasks":tasks})
+    print("entro index",request.session["tasks"])
+    if "tasks" not in request.session:
+        request.session["tasks"]=[]
+    return render(request,"tasks/index.html",{"tasks":request.session["tasks"]})
+
 def add(request):
+    print("entro post")
     #si el metodo es post, o se esta enviando. Lo que se hace es crear un nuevo formulario con toda la informacion
     #de lo que se esta enviando
     #si es POST entonces copiar a la list
@@ -22,7 +26,9 @@ def add(request):
             #toma la data que es tarea
             task=form.cleaned_data["task"]
             #lo agrego a la lista
-            tasks.append(task)
+            request.session["tasks"].append(task)
+           # request.session["tasks"]+=[task]
+            print("asi quedo la lista en add",request.session["tasks"])
             #Automaticamente refresa a la otra pagina
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
