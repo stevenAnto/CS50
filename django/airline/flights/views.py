0eebbbbb77603dc85e82bce8d19a8769fd278247
stevenAnto <ecalcinap@unsa.edu.pe>
2023-05-20 20:12:47 -0500

    flight with idpk

--- django/airline/flights/views.py
+++ django/airline/flights/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Flight
 
+def index(request):
+    return render(request,"flights/index.html",{"flights":Flight.objects.all()})
+
+
+def flight(request, flight_id):
+    print("enftro flight")
+
+    flight = Flight.objects.get(id=flight_id)
+    return render(request, "flights/flight.html",
+            {"flight":flight}
+            )
+    #como hay una relacion de muchos a muchos
 # Create your views here.
