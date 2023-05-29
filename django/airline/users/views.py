db0143b104a129b2839701bb998fe7a96ba7387c
stevenAnto <ecalcinap@unsa.edu.pe>
2023-05-29 17:24:21 -0500

    logeando

--- django/airline/users/views.py
+++ django/airline/users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -7,6 +8,21 @@ def index(request):
         return HttpResponseRedirect(reverse("login"))
 
 def login_view(request):
+    print("solo entro a login_view")
+    if request.method =="POST":
+        print("entro con post")
+        username=request.POST["username"]
+        password=request.POST["password"]
+        user = authenticate(request,username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request,"users/login.html",{
+                "message":"invalidad credentaial"
+                }
+                    )
+
     return render(request,"users/login.html")
 
 def logout_view(request):
